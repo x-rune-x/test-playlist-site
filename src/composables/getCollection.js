23cbcbbd534f537cbea8644a3b91dd43e206cbec
James Bedford <jamesbedford@protonmail.com>
2022-03-04 21:35:46 +1100

    Added composables.

---
+++ src/composables/getCollection.js
@@ -0,0 +1,32 @@
+import { ref, watchEffect } from "vue";
+import { projectFirestore } from "../firebase/config";
+
+const getCollection = (collection) => {
+  const documents = ref(null)
+  const error = ref(null)
+
+  let collectionRef = projectFirestore.collection(collection)
+    .orderBy('createdAt')
+
+  const unsub = collectionRef.onSnapshot((snap) => {
+    console.log('snapshot')
+    let results = []
+    snap.docs.forEach(doc => {
+      doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+    })
+    documents.value = results
+    error.value = null
+  }, (err) => {
+    console.log(err.message)
+    documents.value = null
+    error.value = 'Could not fetch data.'
+  })
+
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => unsub())
+  })
+
+  return { documents, error }
+}
+
+export default getCollection
---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5h1q_BjhahovaF07evLjP5HG0GjF1Xl8",
+  authDomain: "playlist-test-site.firebaseapp.com",
+  projectId: "playlist-test-site",
+  storageBucket: "playlist-test-site.appspot.com",
+  messagingSenderId: "844500256250",
+  appId: "1:844500256250:web:3d8963e036416011d9d043"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
