ba4541fe4306a1760487e86506a9e90a81915056
James Bedford <jamesbedford@protonmail.com>
2022-03-14 11:43:17 +1100

    Homepage displays list of playlist titles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="home">
     <p>Home page</p>
+    <div v-if="error">
+      Could not fetch the data
+      {{ error.value }}
+    </div>
+    <div v-else>
+      <p v-for="doc in documents" :key="doc.id">{{ doc.title }}</p>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
+import getCollection from '../composables/getCollection'
+
+export default { 
+  name: 'Home',
+  setup() {
+    const { documents, error } = getCollection('playlists')
+
+    return { documents, error }
+  }
 }
 </script>
